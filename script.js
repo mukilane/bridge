db7ffd346c53a9b127c92f22e6db0a26baf6b0bd
mukilane <mukilane@gmail.com>
2017-02-18 22:41:54 +0530

    Icons

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ app.controller('main', function($scope) {
 	grid.addEventListener('click', function(event) {
 		if(event.target.classList.contains("md-button"))  {
 			event.target.classList.toggle('raised');
-			
 		} else if (event.target.tagName == 'IMG') {
 			event.target.parentNode.classList.toggle('raised');
-
 		}
  	}, false);
 });
