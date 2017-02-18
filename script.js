9bc65a66a3b2827962bb812b871ec187a97f9893
mukilane <mukilane@gmail.com>
2017-02-18 22:30:27 +0530

    First Build

---
+++ script.js
@@ -0,0 +1,13 @@
+var app = angular.module('bridge', ['ngMaterial']);
+app.controller('main', function($scope) {
+	var grid = document.getElementById('grid');
+	grid.addEventListener('click', function(event) {
+		if(event.target.classList.contains("md-button"))  {
+			event.target.classList.toggle('raised');
+			
+		} else if (event.target.tagName == 'IMG') {
+			event.target.parentNode.classList.toggle('raised');
+
+		}
+ 	}, false);
+});
